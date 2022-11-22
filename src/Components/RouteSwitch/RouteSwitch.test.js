3e7847e4032a31ede30e0b728f6f47ebd401f848
KP <kpinc425@gmail.com>
2022-11-21 22:50:52 -0800

    Implement testing for RouteSwitch component

--- src/Components/RouteSwitch/RouteSwitch.test.js
+++ src/Components/RouteSwitch/RouteSwitch.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import RouteSwitch from './RouteSwitch';
 
 describe("RouteSwitch Component", () => {
@@ -6,4 +7,24 @@ describe("RouteSwitch Component", () => {
         const { container } = render(<RouteSwitch />);
         expect(container).toMatchSnapshot();
     });
+
+    it("renders nav initialy", () => {
+        render(<RouteSwitch />);
+        expect(screen.getByRole('navigation')).toBeTruthy();
+    })
+
+    it("renders Home Page initialy", () => {
+        render(<RouteSwitch />);
+        expect(screen.getByRole('heading').textContent).toMatch(/Home Page/i);
+    })
+
+    // THIS SHOULD TEST NAV
+    // it("render shop page after link clicked", () => {
+    //     render(<RouteSwitch />);
+    //     const link = screen.getByRole("link", { name: "Shop" });
+    //     // console.log(link);
+    //     userEvent.click(link);
+
+    //     expect(screen.getByRole("heading").textContent).toMatch(/Shop Page/i);
+    // })
 })
